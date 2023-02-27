3cda004a39557a8fba16ee995a6c81761fe5f4b0
catmiih <emilyleme.dev@gmail.com>
2023-02-27 12:26:04 -0300

    feat: implement /users route

---
+++ src/main/java/com/emily/springproject/resources/UserResource.java
@@ -0,0 +1,19 @@
+package com.emily.springproject.resources;
+
+import com.emily.springproject.entities.User;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResource {
+
+    @GetMapping
+    public ResponseEntity<User> findAll() {
+        User u = new User(1L, "Maria", "maria@gmail.com", "9999999", "123");
+        return ResponseEntity.ok().body(u);
+    }
+}
