2640bbf6be08bcde8544830b9dadf68c07abce56
catmiih <emilyleme.dev@gmail.com>
2023-02-27 12:50:58 -0300

    feat: JPA Configuration, add dependences & create/config database

--- src/main/java/com/emily/springproject/entities/User.java
+++ src/main/java/com/emily/springproject/entities/User.java
@@ -1,12 +1,19 @@
 package com.emily.springproject.entities;
 
+import jakarta.persistence.*;
+
 import java.io.Serializable;
 
+@Entity
+@Table(name = "tb_user")
 public class User implements Serializable {
 
     private static final long serialVersionUID = 1L;
 
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private Long id;
+
     private String name;
     private String email;
     private String phone;
